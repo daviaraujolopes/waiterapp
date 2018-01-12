088d79c3db2121de2ed4d53a63b8a642d96d816b
Fernando-NoteG\Fernando <fernando.naghar@gmail.com>
2018-01-11 23:16:26 -0200

    commit evolucao no tutorial

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,39 +1,61 @@
 // Libs
 import 'angular-animate';
 import 'angular-meteor';
+import 'angular-meteor-auth';
 import 'angular-sanitize';
 import 'angular-ui-router';
+import 'angular-moment';
 import 'ionic-scripts';
 import Angular from 'angular';
 import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
- 
-// Modules
-import ChatsCtrl from '../controllers/chats.controller';
-import CalendarFilter from '../filters/calendar.filter';
-import RoutesConfig from './routes';
 
- const App = 'Waiterapp';
- 
+// Controllers
+import ProfileCtrl from '../controllers/profile.controller';
+import LoginCtrl from '../controllers/login.controller';
+import SettingsCtrl from '../controllers/settings.controller';
+import ConfirmationCtrl from '../controllers/confirmation.controller';
+import ChatsCtrl from '../controllers/chats.controller';
+import ChatCtrl from '../controllers/chat.controller';
+
+
+//filters
+import CalendarFilter from '../filters/calendar.filter';
+
+//directives
+import InputDirective from '../directives/input.directive';
+
+//config
+import Routes from './routes';
+
+const App = 'Waiterapp';
+
 // App
 Angular.module(App, [
-  'angular-meteor',
-  'ionic'
+    'angular-meteor',
+    'angular-meteor.auth',
+    'angularMoment',
+    'ionic'
 ]);
- 
+
 new Loader(App)
     .load(ChatsCtrl)
+    .load(ChatCtrl)
+    .load(LoginCtrl)
+    .load(SettingsCtrl)
+    .load(ProfileCtrl)
+    .load(ConfirmationCtrl)
     .load(CalendarFilter)
-    .load(RoutesConfig);
+    .load(InputDirective)
+    .load(Routes);
 
 // Startup
 if (Meteor.isCordova) {
-  Angular.element(document).on('deviceready', onReady);
+    Angular.element(document).on('deviceready', onReady);
+} else {
+    Angular.element(document).ready(onReady);
 }
-else {
-  Angular.element(document).ready(onReady);
-}
- 
+
 function onReady() {
-  Angular.bootstrap(document, [App]);
+    Angular.bootstrap(document, [App]);
 }
